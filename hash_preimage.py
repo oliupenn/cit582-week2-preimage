9af3d937d73a6130ae444ed4f9bb5d5ad4c5b5a0
oliupenn <52123655+oliupenn@users.noreply.github.com>
2022-01-18 21:57:54 +0800

    Update hash_preimage.py

--- hash_preimage.py
+++ hash_preimage.py
@@ -5,28 +5,24 @@ def hash_preimage(target_string):
     if not all( [x in '01' for x in target_string ] ):
         print( "Input should be a string of bits" )
         return
-    matched = False
-    while not matched:
-        txt = os.urandom(2)
-        hashed_txt = hashlib.sha256(txt).hexdigest()
-        l = len(target_string)
-        if l % 4 == 0:
-            num_digits_hex = int(l/4)
-        else:
-            num_digits_hex = int(l/4)+1
-        hashed_trimmed_txt = hashed_txt[-num_digits_hex:]
-        x = hex_to_bin(hashed_trimmed_txt)
-        x_same_length_target = x[-l:]
-        if is_matched(x,target_string):
-            matched = True
-    return txt
-
-def is_matched(x,y):
-    res = True
-    for i in range(len(y)):
-        if y[i] != x[i]:
-            res = False
-    return res
+    else:
+        matched = False
+        while not matched:
+            txt = os.urandom(1)
+            hashed_txt = hashlib.sha256(txt).hexdigest()
+            l = len(target_string)
+            if l % 4 == 0:
+                num_digits_hex = int(l/4)
+            else:
+                num_digits_hex = int(l/4)+1
+            hashed_trimmed_txt = hashed_txt[-num_digits_hex:]
+            print(hashed_trimmed_txt)
+            x = hex_to_bin(hashed_trimmed_txt)
+            x_same_length_target = x[-l:]
+            print(x_same_length_target)
+            if x_same_length_target == target_string:
+                matched = True
+                return txt
 
 def hex_to_bin(x):
     length = len(x)
@@ -38,3 +34,7 @@ def hex_to_bin(x):
         for i in range(length_diff):
             res = '0'+res       
     return res
+
+if __name__ == "__main__":
+    hash_preimage('0111101')
+    
